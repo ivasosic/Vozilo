434730e413fdded8a6343c898e0b90be6d02ae65
a <145933200+ivasosic@users.noreply.github.com>
2024-01-09 11:28:18 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vozilo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            klasaVozilo Motocikl1 = new klasaVozilo("Yamaha", 310, 2);
+            klasaVozilo Automobil = new klasaVozilo("Nissan", 380, 4);
+
+
+            Console.WriteLine(Motocikl1.ToString());
+            Console.WriteLine(Automobil.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ klasaVozilo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vozilo
+{
+    internal class klasaVozilo
+    {
+        string vrsta;
+        int max_brzina;
+        int br_kotaca;
+
+        public string Vrsta { get => vrsta; set => vrsta = value; }
+        public int Max_brzina { get => max_brzina; set => max_brzina = value; }
+        public int Br_kotaca { get => br_kotaca; set => br_kotaca = value; }
+
+        public klasaVozilo(string vrsta, int max_brzina, int br_kotaca)
+        {
+            this.Vrsta = vrsta;
+            this.Max_brzina = max_brzina;
+            this.Br_kotaca = br_kotaca;
+        }
+
+        public klasaVozilo()
+        {
+            this.Vrsta = vrsta;
+            this.Max_brzina = max_brzina;
+            this.Br_kotaca = br_kotaca;
+
+        }
+
+        public override string ToString()
+        {
+            string str = "Vrsta: " + vrsta + "\n Broj kotača: " + br_kotaca + "\n Max brzina: " + max_brzina;
+            return str;
+        }
+
+    }
+}
